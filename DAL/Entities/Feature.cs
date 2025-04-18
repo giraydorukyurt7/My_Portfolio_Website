b4a0725fac1bf675e118e7285e5710e137ebd81a
Giray Doruk Yurtseven <giraysoftwareengineer3.14@hotmail.com>
2025-04-18 23:11:49 +0300

    Initial Commit

---
+++ DAL/Entities/Feature.cs
@@ -0,0 +1,9 @@
+﻿namespace My_Portfolio_Website.DAL.Entities
+{
+    public class Feature
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace My_Portfolio_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
