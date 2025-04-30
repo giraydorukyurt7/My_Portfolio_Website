b963d51b1e7a39dde013738376d4b939d9fa2464
Giray Doruk Yurtseven <giraysoftwareengineer3.14@hotmail.com>
2025-04-30 23:52:15 +0300

    Making website Dynamic 1

--- ViewComponents/_FeatureComponentPartial.cs
+++ ViewComponents/_FeatureComponentPartial.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using My_Portfolio_Website.DAL.Context;
 
 namespace My_Portfolio_Website.ViewComponents
 {
     public class _FeatureComponentPartial : ViewComponent
     {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = portfolioContext.Features.ToList();
+            return View(values);
         }
     }
 }
