fb9827f37dbfa32ec41e324c5a3924d9f0d9a54a
Giray Doruk Yurtseven <giraysoftwareengineer3.14@hotmail.com>
2025-05-05 01:41:35 +0300

    SQL connection 2

--- obj/Debug/net9.0/My_Portfolio_Website.AssemblyInfo.cs
+++ obj/Debug/net9.0/My_Portfolio_Website.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My_Portfolio_Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e64ba2850679a42ef5ad95f73447b8c6bfc3d8b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c57556942a98b573eb3edb7d41a0a6a1e7e4654")]
 [assembly: System.Reflection.AssemblyProductAttribute("My_Portfolio_Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My_Portfolio_Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
